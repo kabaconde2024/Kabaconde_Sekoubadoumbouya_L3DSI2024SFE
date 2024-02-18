4b336482ae5db4f373608ea09fcf306b27f9f13c
SekoubaD <sekoubadoumbouya030@gmail.com>
2024-02-18 22:05:35 +0100

    modif apres l'implementation de login

--- Back_end/controller/authController.js
+++ Back_end/controller/authController.js
@@ -2,6 +2,20 @@
 const User = require('../model/User');
 const jwt = require('jsonwebtoken');
 
+// Fonction pour récupérer toutes les données d'utilisateurs
+const getAllUsers = async (req, res) => {
+    try {
+        // Récupérer tous les utilisateurs depuis la base de données
+        const users = await User.find();
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur lors de la récupération des utilisateurs." });
+    }
+};
+
+// Fonction pour enregistrer un nouvel utilisateur
 const register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -30,4 +44,24 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Recherche de l'utilisateur dans la base de données par email
+        const user = await User.findOne({ email });
+
+        // Vérification de l'existence de l'utilisateur et de la correspondance du mot de passe
+        if (!user || user.password !== password) {
+            return res.status(401).json({ message: 'Identifiants incorrects.' });
+        }
+
+        // Authentification réussie
+        res.status(200).json({ message: 'Connexion réussie.', user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erreur lors de la connexion.' });
+    }
+};
+
+module.exports = { register, getAllUsers, login };
--- Back_end/routes/auth.js
+++ Back_end/routes/auth.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login } = require('../controller/authController');
+// Importez la fonction getAllUsers depuis le contrôleur authController
+const { register, getAllUsers, login } = require('../controller/authController');
 
+// Définissez la route pour l'enregistrement d'un nouvel utilisateur
 router.post('/register', register);
+
+// Définissez la route pour récupérer tous les utilisateurs
+router.get('/users', getAllUsers);
+router.post('/login', login);
 
 module.exports = router;
