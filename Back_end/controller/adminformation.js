809085238e86e8ad8e1e5eab0c91afa74de60e3d
kabaconde2024 <kabaconde5259@gmail.com>
2024-02-23 03:47:43 +0100

    modification sur mesformations

--- Back_end/controller/adminformation.js
+++ Back_end/controller/adminformation.js
@@ -46,8 +46,52 @@ const afficherFormations = async (req, res) => {
     }
   };
 
+  const updateFormation = async (req, res) => {
+    try {
+        const { id } = req.params; // Récupérer l'ID à partir des paramètres de l'URL
+        const { titre, description, date, prix } = req.body;
+
+        // Vérifier si la formation existe
+        const existingFormation = await Formation.findById(id);
+
+        if (!existingFormation) {
+            return res.status(404).json({ message: 'Formation non trouvée.' });
+        }
+
+        // Mettre à jour les propriétés de la formation
+        existingFormation.titre = titre || existingFormation.titre;
+        existingFormation.description = description || existingFormation.description;
+        existingFormation.date = date || existingFormation.date;
+        existingFormation.prix = prix || existingFormation.prix;
+
+        // Sauvegarder les modifications dans la base de données
+        await existingFormation.save();
+
+        res.status(200).json({ message: 'Formation mise à jour avec succès.' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erreur lors de la mise à jour de la formation.' });
+    }
+};
+
+const deleteFormationById = async (req, res) => {
+    try {
+        const resultat = await Formation.deleteOne({ _id: req.params.id });
+
+        if (resultat.deletedCount === 0) {
+            return res.status(404).json({ message: "Formation non trouvée." });
+        }
+
+        res.status(200).json({ message: 'Formation supprimée avec succès.' });
+    } catch (erreur) {
+        console.error(erreur);
+        res.status(500).json({ message: "Erreur lors de la suppression de la formation." });
+    }
+};
 
 module.exports = {
     creerFormation,
     afficherFormations,
+    updateFormation,
+    deleteFormationById,
 }
--- Back_end/routes/formation.js
+++ Back_end/routes/formation.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // Importez la fonction getAllUsers depuis le contrôleur authController
-const { creerFormation, afficherFormations} = require('../controller/adminformation');
+const { creerFormation, afficherFormations,updateFormation,deleteFormationById} = require('../controller/adminformation');
 router.post('/creerFormation', creerFormation);
 router.get('/afficherFormations', afficherFormations);
+router.put('/updateFormation/:id', updateFormation);
+router.delete('/deleteFormationById/:id', deleteFormationById);
+
+
+
 module.exports = router;
 
