5de70968d057a9b20e9e98eba4fbf1fc4cc0073c
SekoubaD <sekoubadoumbouya030@gmail.com>
2024-02-27 15:37:02 +0100

    apres l'ajout du modal voir details

--- Back_end/controller/userFormationController.js
+++ Back_end/controller/userFormationController.js
@@ -1,29 +1,44 @@
 const UserFormation = require('../model/UserFormation');
 
-
 const ajouterUserFormation = async (req, res) => {
     const { idUtilisateur, idFormation, titreFormation } = req.body;
 
+    // Vérifier si toutes les données nécessaires sont fournies
+    if (!idUtilisateur || !idFormation || !titreFormation) {
+        return res.status(400).json({ message: 'Veuillez fournir toutes les informations nécessaires pour ajouter une nouvelle formation utilisateur.' });
+    }
+
     try {
+        // Créer une nouvelle formation utilisateur
         const userFormation = new UserFormation({
             idUtilisateur,
             idFormation,
             titreFormation
         });
 
+        // Enregistrer la nouvelle formation utilisateur dans la base de données
         const newUserFormation = await userFormation.save();
+
+        // Répondre avec la nouvelle formation utilisateur créée
         res.status(201).json(newUserFormation);
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        // Gérer les erreurs lors de l'ajout de la formation utilisateur
+        console.error('Erreur lors de l\'ajout de la formation utilisateur :', error.message);
+        res.status(500).json({ message: 'Erreur lors de l\'ajout de la formation utilisateur.' });
     }
 };
+
 const getUserFormations = async (req, res) => {
     try {
-        // Utilisez la méthode find() du modèle UserFormation pour récupérer toutes les formations d'utilisateur
+        // Récupérer toutes les formations utilisateur depuis la base de données
         const userFormations = await UserFormation.find();
+
+        // Répondre avec toutes les formations utilisateur
         res.status(200).json(userFormations);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        // Gérer les erreurs lors de la récupération des formations utilisateur
+        console.error('Erreur lors de la récupération des formations utilisateur :', error.message);
+        res.status(500).json({ message: 'Erreur lors de la récupération des formations utilisateur.' });
     }
 };
 
