841d307f424183579883b9d0fa67922edf9c1142
SekoubaD <sekoubadoumbouya030@gmail.com>
2024-03-11 01:54:10 +0100

    ajustement

--- Back_end/model/Formation.js
+++ Back_end/model/Formation.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 const formationSchema = new Schema({
     titre: { type: String, required: true },
     description: { type: String, required: true },
-    date: { type: String, required: true },
+    dateDebut: { type: Date, required: true }, 
+    dateFin: { type: Date, required: true },  
     prix: { type: Number, required: true },
     etat: { type: [String], default: ["En cours"] },
     verifie: { type: Boolean, default: false }, 
     sessions: [{ type: Schema.Types.ObjectId, ref: 'Session' }],
-
 });
 
 module.exports = mongoose.model('Formation', formationSchema);
