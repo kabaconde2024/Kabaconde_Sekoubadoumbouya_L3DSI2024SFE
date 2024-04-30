637489314b809dac62fc9ff1383b9702f7bcbf06
SekoubaD <sekoubadoumbouya030@gmail.com>
2024-04-30 13:46:23 +0100

    Ajustement

--- Back_end/controller/adhesionController.js
+++ Back_end/controller/adhesionController.js
@@ -140,15 +140,7 @@ const getDemande = async (req, res) => {
     res.status(500).json({ success: false, message: 'Erreur lors de la récupération des demandes.' });
   }
 };
-router.get('/historique', async (req, res) => {
-  try {
-    const historique = await getDemande();
-    res.json(historique);
-  } catch (error) {
-    console.error('Erreur lors de la récupération de l\'historique des demandes :', error);
-    res.status(500).json({ success: false, message: 'Erreur lors de la récupération de l\'historique des demandes.' });
-  }
-});
+
 
 const getDemandesByUser = async (req, res) => {
   const { userId } = req.params;
