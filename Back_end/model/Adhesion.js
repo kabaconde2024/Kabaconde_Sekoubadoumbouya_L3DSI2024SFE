e32c9944e9a168aee61e1cf644e97fa53511aad7
conde <kabaconde5259@gmail.com>
2025-01-03 04:23:57 +0100

    j'ai ajoute une petite modification

--- Back_end/model/Adhesion.js
+++ Back_end/model/Adhesion.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 const demandeSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', unique: true, sparse: true },
     accepte: { type: Boolean, default: false },
+    result: { type: Boolean, default: false },
+
     paye: { type: Boolean, default: false },
     message: { type: String, default: "vous n'avez pas fait votre adhesion !" },
     dateDemande: { type: Date, default: Date.now } // Champ pour enregistrer la date de la demande
